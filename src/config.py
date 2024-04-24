783479b2385a005384912000cc4d3036c6dab4da
Winspain <penndicardo@outlook.com>
2024-04-24 19:40:41 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,13 +1,15 @@
-from peewee import MySQLDatabase
+from peewee import PooledMySQLDatabase
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DB = MySQLDatabase(
+DB = PooledMySQLDatabase(
     os.getenv("DB_NAME"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     host=os.getenv("DB_HOST"),
     port=int(os.getenv("DB_PORT"))
+    max_connections=8,  # 最大连接数
+    stale_timeout=300,  # 连接在这个时间内未被使用则被关闭
 )
