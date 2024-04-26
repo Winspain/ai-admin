6a7629b4c3e5915a28b2492562b32fad6ce765c2
winspain <penndicardo@outlook.com>
2024-04-26 17:34:53 +0800

    refactor

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI, HTTPException
+import os
+from pathlib import Path
 
-from src.database import register_tortoise
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
 from src.auth import router
+from src.database import register_tortoise
+
+# 加载.env文件
+load_dotenv()
 
 app = FastAPI()
 
@@ -10,3 +18,9 @@ register_tortoise(app)
 
 # 添加路由
 app.include_router(router.router)
+
+if __name__ == '__main__':
+    web_port = int(os.getenv('WEB_PORT'))
+    current_path = Path(__file__).parent.parent
+    log_path = f'{current_path}/logging.json'
+    uvicorn.run('main:app', host='0.0.0.0', port=web_port, log_config=log_path)
